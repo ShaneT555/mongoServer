49fae7629f360f1885af6ec40bd21390f1e8a9e9
Shane <179925883+ShaneT555@users.noreply.github.com>
2024-10-09 12:06:16 +0100

    Framework for this weeks project

--- src/server.js
+++ src/server.js
@@ -9,7 +9,50 @@ require("dotenv").config();
 // This line imports and runs dotenv in one line
 require("./db/connection");
 
-const Book = require("./db/models/bookmodel")
+const Book = require("./db/models/bookmodel");
+
+app.get("/listBooks",
+    async function listBooks(req,res) {
+        try {
+          const output = await Book.find({})
+            res.status(200).json(output);
+        } catch (error) {
+            console.log(error);
+            const responseMessage = {
+                message: `Unable to find book list`
+            }
+            res.status(500).json(responseMessage)
+        }
+    }
+)
+
+app.post("/addBook", async (req,res) => {
+    try {
+        const result = await Book.create(
+            {
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre
+            }
+        )
+        console.log(result);
+        const responseMessage = {
+            message: `Book ${req.body.title} has been added to the database`
+        }
+        res.status(201);
+        res.json(responseMessage)
+    } catch (error) {
+        console.log(error);
+        const responseMessage = {
+            message: `Book ${req.body.title} was not added`,
+            DBresponse: error
+        }
+        res.status(418)
+        res.json(responseMessage)
+    }
+}
+
+)
 
 app.use("/health", (req,res) => {res.send("API is healthy")});
 // health route to verify server
